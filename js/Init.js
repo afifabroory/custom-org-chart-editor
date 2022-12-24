68a6c199febd42419c487517c3270df908799797
Muhammad Afif Abroory <afif.abroory@gmail.com>
2022-12-24 12:49:23 +0800

    Testing purposes

--- js/Init.js
+++ js/Init.js
@@ -6,8 +6,9 @@ window.MAX_REQUEST_SIZE = window.MAX_REQUEST_SIZE  || 10485760;
 window.MAX_AREA = window.MAX_AREA || 15000 * 15000;
 
 // URLs for save and export
-window.EXPORT_URL = window.EXPORT_URL || '/export';
-window.SAVE_URL = window.SAVE_URL || '/save';
+// D:\development\XAMPP\htdocs\mxgraph-php\examples
+window.EXPORT_URL = 'http://localhost/mxgraph-php/examples/deployment.php';
+window.SAVE_URL = 'http://localhost/mxgraph-php/examples/deployment.php';
 window.OPEN_URL = window.OPEN_URL || '/open';
 window.RESOURCES_PATH = window.RESOURCES_PATH || 'resources';
 window.RESOURCE_BASE = window.RESOURCE_BASE || window.RESOURCES_PATH + '/grapheditor';
@@ -24,6 +25,6 @@ window.OPEN_FORM = window.OPEN_FORM || 'open.html';
 // files (the special bundle and the default bundle) is disabled to
 // save a GET request. This requires that all resources be present in
 // each properties file since only one file is loaded.
-window.mxBasePath = window.mxBasePath || '../../../src';
+window.mxBasePath = window.mxBasePath || '';
 window.mxLanguage = window.mxLanguage || urlParams['lang'];
 window.mxLanguages = window.mxLanguages || ['de', 'se'];
