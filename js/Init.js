7c2728fe91b2dc77031d56445ad12258c7b30bfd
Muhammad Afif Abroory <afif.abroory@gmail.com>
2022-12-26 21:35:15 +0800

    Minify

--- js/Init.js
+++ js/Init.js
@@ -1,12 +1,8 @@
-// urlParams is null when used for embedding
 window.urlParams = window.urlParams || {};
 
-// Public global variables
 window.MAX_REQUEST_SIZE = window.MAX_REQUEST_SIZE  || 10485760;
 window.MAX_AREA = window.MAX_AREA || 15000 * 15000;
 
-// URLs for save and export
-// D:\development\XAMPP\htdocs\mxgraph-php\examples
 window.EXPORT_URL = 'http://localhost/mxgraph-php/examples/deployment.php';
 window.SAVE_URL = 'http://localhost/mxgraph-php/examples/deployment.php';
 window.OPEN_URL = window.OPEN_URL || '/open';
@@ -18,13 +14,6 @@ window.STYLE_PATH = window.STYLE_PATH || 'styles';
 window.CSS_PATH = window.CSS_PATH || 'styles';
 window.OPEN_FORM = window.OPEN_FORM || 'open.html';
 
-// Sets the base path, the UI language via URL param and configures the
-// supported languages to avoid 404s. The loading of all core language
-// resources is disabled as all required resources are in grapheditor.
-// properties. Note that in this example the loading of two resource
-// files (the special bundle and the default bundle) is disabled to
-// save a GET request. This requires that all resources be present in
-// each properties file since only one file is loaded.
 window.mxBasePath = window.mxBasePath || '';
 window.mxLanguage = window.mxLanguage || urlParams['lang'];
 window.mxLanguages = window.mxLanguages || ['de', 'se'];
