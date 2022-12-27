78e85076af8f201e14c856f9d1dbfbca335c909d
Muhammad Afif Abroory <afif.abroory@gmail.com>
2022-12-27 10:29:07 +0800

    Merge
    commit ca1cc5720ebe2a8b18955431b513da67c38647c8
    Author: Muhammad Afif Abroory <afif.abroory@gmail.com>
    Date:   Tue Dec 27 09:54:53 2022 +0800
    
        Update

--- js/Init.js
+++ js/Init.js
@@ -6,14 +6,21 @@ window.MAX_AREA = window.MAX_AREA || 15000 * 15000;
 window.EXPORT_URL = 'http://localhost/mxgraph-php/examples/deployment.php';
 window.SAVE_URL = 'http://localhost/mxgraph-php/examples/deployment.php';
 window.OPEN_URL = window.OPEN_URL || '/open';
-window.RESOURCES_PATH = window.RESOURCES_PATH || 'resources';
+window.RESOURCES_PATH = window.RESOURCES_PATH || '/org-editor/resources';
 window.RESOURCE_BASE = window.RESOURCE_BASE || window.RESOURCES_PATH + '/grapheditor';
-window.STENCIL_PATH = window.STENCIL_PATH || 'stencils';
-window.IMAGE_PATH = window.IMAGE_PATH || 'images';
-window.STYLE_PATH = window.STYLE_PATH || 'styles';
-window.CSS_PATH = window.CSS_PATH || 'styles';
+window.STENCIL_PATH = window.STENCIL_PATH || '/org-editor/stencils';
+window.IMAGE_PATH = window.IMAGE_PATH || '/org-editor/images';
+window.STYLE_PATH = window.STYLE_PATH || '/org-editor/styles';
+window.CSS_PATH = window.CSS_PATH || '/org-editor/styles';
 window.OPEN_FORM = window.OPEN_FORM || 'open.html';
 
-window.mxBasePath = window.mxBasePath || '';
+// Sets the base path, the UI language via URL param and configures the
+// supported languages to avoid 404s. The loading of all core language
+// resources is disabled as all required resources are in grapheditor.
+// properties. Note that in this example the loading of two resource
+// files (the special bundle and the default bundle) is disabled to
+// save a GET request. This requires that all resources be present in
+// each properties file since only one file is loaded.
+window.mxBasePath = window.mxBasePath || '/org-editor/';
 window.mxLanguage = window.mxLanguage || urlParams['lang'];
 window.mxLanguages = window.mxLanguages || ['de', 'se'];
