ca1cc5720ebe2a8b18955431b513da67c38647c8
Muhammad Afif Abroory <afif.abroory@gmail.com>
2022-12-27 09:54:53 +0800

    Update

--- js/Init.js
+++ js/Init.js
@@ -10,12 +10,12 @@ window.MAX_AREA = window.MAX_AREA || 15000 * 15000;
 window.EXPORT_URL = 'http://localhost/mxgraph-php/examples/deployment.php';
 window.SAVE_URL = 'http://localhost/mxgraph-php/examples/deployment.php';
 window.OPEN_URL = window.OPEN_URL || '/open';
-window.RESOURCES_PATH = window.RESOURCES_PATH || 'resources';
+window.RESOURCES_PATH = window.RESOURCES_PATH || '/org-editor/resources';
 window.RESOURCE_BASE = window.RESOURCE_BASE || window.RESOURCES_PATH + '/grapheditor';
-window.STENCIL_PATH = window.STENCIL_PATH || 'stencils';
-window.IMAGE_PATH = window.IMAGE_PATH || 'images';
-window.STYLE_PATH = window.STYLE_PATH || 'styles';
-window.CSS_PATH = window.CSS_PATH || 'styles';
+window.STENCIL_PATH = window.STENCIL_PATH || '/org-editor/stencils';
+window.IMAGE_PATH = window.IMAGE_PATH || '/org-editor/images';
+window.STYLE_PATH = window.STYLE_PATH || '/org-editor/styles';
+window.CSS_PATH = window.CSS_PATH || '/org-editor/styles';
 window.OPEN_FORM = window.OPEN_FORM || 'open.html';
 
 // Sets the base path, the UI language via URL param and configures the
@@ -25,6 +25,6 @@ window.OPEN_FORM = window.OPEN_FORM || 'open.html';
 // files (the special bundle and the default bundle) is disabled to
 // save a GET request. This requires that all resources be present in
 // each properties file since only one file is loaded.
-window.mxBasePath = window.mxBasePath || '';
+window.mxBasePath = window.mxBasePath || '/org-editor/';
 window.mxLanguage = window.mxLanguage || urlParams['lang'];
 window.mxLanguages = window.mxLanguages || ['de', 'se'];
