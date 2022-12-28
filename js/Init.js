f1033de68c31e6a0d0271610fdb0b210c992682e
Muhammad Afif Abroory <afif.abroory@gmail.com>
2022-12-28 12:06:17 +0800

    Removing unecessary things

--- js/Init.js
+++ js/Init.js
@@ -1,3 +1,7 @@
+/*
+ * Modified version of the mxgraph GraphEditor. 
+ * 2022, Muhammad Afif Abroory (https://github.com/afifabroory/org-chart-editor2)
+ */  
 window.urlParams = window.urlParams || {};
 
 window.MAX_REQUEST_SIZE = window.MAX_REQUEST_SIZE  || 10485760;
@@ -12,15 +16,8 @@ window.STENCIL_PATH = window.STENCIL_PATH || '/org-editor/stencils';
 window.IMAGE_PATH = window.IMAGE_PATH || '/org-editor/images';
 window.STYLE_PATH = window.STYLE_PATH || '/org-editor/styles';
 window.CSS_PATH = window.CSS_PATH || '/org-editor/styles';
-window.OPEN_FORM = window.OPEN_FORM || 'open.html';
+window.OPEN_FORM = window.OPEN_FORM || '';
 
-// Sets the base path, the UI language via URL param and configures the
-// supported languages to avoid 404s. The loading of all core language
-// resources is disabled as all required resources are in grapheditor.
-// properties. Note that in this example the loading of two resource
-// files (the special bundle and the default bundle) is disabled to
-// save a GET request. This requires that all resources be present in
-// each properties file since only one file is loaded.
 window.mxBasePath = window.mxBasePath || '/org-editor/';
 window.mxLanguage = window.mxLanguage || urlParams['lang'];
-window.mxLanguages = window.mxLanguages || ['de', 'se'];
+window.mxLanguages = window.mxLanguages || [];
